acd28c690b6d45473f3c3d0d7abc8bfa3a5231f3
RealmsEnd <chriscrawford360@outlook.com>
2022-07-20 09:16:03 -0500

    corsAdded

--- src/main/java/com/summitworks/controller/HomeController.java
+++ src/main/java/com/summitworks/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.summitworks.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-@RequestMapping("/login")
 public class HomeController {
     
     @Autowired
     private HomeService homeService;
 
-    @GetMapping
-    public String home() {
+    @GetMapping("/")
+    public String home() { // Will direct to index.html ---------------
         return "Hello World";
     }
 
-    @PostMapping
+    @PostMapping("/login")
     public String login(@RequestParam("email") String email, @RequestParam("password") String password) {
         return homeService.login(email, password);
     }
